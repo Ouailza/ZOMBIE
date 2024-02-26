6180dca9eb7fece01d99b0b691c122aecd8caad9
Ouailza <155404467+Ouailza@users.noreply.github.com>
2024-02-26 18:16:51 +0100

    Update ZOMBIE.py

--- ZOMBIE.py
+++ ZOMBIE.py
@@ -10,5 +10,6 @@ print('\033[1;32m'"""  ███████╗░█████╗░███
 print('\033[1;36m''~'*50)
 print('\033[1;37m''TOOL :''\033[1;31m'' ZOMBIE')
 print('\033[1;37m''TYPE :''\033[1;32m'' PAID')
-print('\033[1;37m''VERSION :'\033[1;33m'' 0.1')
+print('\033[1;37m''VERSION :''\033[1;33m'' 0.1')
 print('\033[1;37m''DEVELOPER :'' OUAIL')
+
